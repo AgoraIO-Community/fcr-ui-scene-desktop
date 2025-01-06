60145827725a2233a43997a5c1f01fd623940b0f
任一飞 <renyifei@agora.io>
2025-01-06 08:57:42 +0000

    Merge pull request #878 in ADUC/fcr-ui-scene-desktop from update-ci-script-1 to release/1.0.41
    Squashed commit of the following:
    
    commit bc7c17e39d05c0f1589ddc5de3d3e60a90a2d01f
    Author: renyifei <renyifei@agora.io>
    Date:   Mon Jan 6 16:48:45 2025 +0800
    
        chore: update ci script
    
    commit f78d7462f753f51c350084483bfe7587e126341e
    Author: renyifei <renyifei@agora.io>
    Date:   Mon Jan 6 16:36:22 2025 +0800
    
        chore: update ci script

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -4,8 +4,7 @@ build_branch=$fcr_ui_scene_desktop_branch
 
 ci_script_version=v1
 
-. ../apaas-cicd-web/versions.sh
-. ../apaas-cicd-web/utilities/tools.sh
+. ../apaas-cicd-web/utilities/$ci_script_version/tools.sh
 . ../apaas-cicd-web/build/$ci_script_version/dependency.sh
 . ../apaas-cicd-web/build/$ci_script_version/build.sh
 
